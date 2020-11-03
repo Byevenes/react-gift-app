91a67cfd377c89c165be953004624629cc13b2ad
Byevenes <zneel.yevenes@gmail.com>
2020-11-03 17:30:38 -0300

    docs agregado

---
+++ docs/precache-manifest.07be8e642b2f3b8db0285d57b32a1461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53c7b31776bcd2d9d433e8298e53a2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe8ef8f82a1e36e44bd0",
+    "url": "/static/css/main.3d6db0af.chunk.css"
+  },
+  {
+    "revision": "406bf15275c51809c6eb",
+    "url": "/static/js/2.c53e285c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c53e285c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8ef8f82a1e36e44bd0",
+    "url": "/static/js/main.007caa59.chunk.js"
+  },
+  {
+    "revision": "429b012a069fc567e7f4",
+    "url": "/static/js/runtime-main.c670b3c9.js"
+  }
+]);
